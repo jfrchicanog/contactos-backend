7f1633531a80a6b4bc16476e966808c532162448
Francisco Chicano <chicano@lcc.uma.es>
2023-04-23 13:56:04 +0200

    añadido JMS con Artemis externo

--- src/main/java/es/uma/informatica/sii/contactos/ContactosApplication.java
+++ src/main/java/es/uma/informatica/sii/contactos/ContactosApplication.java
@@ -2,11 +2,19 @@ package es.uma.informatica.sii.contactos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.jms.core.MessagePostProcessor;
+
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 
 @SpringBootApplication
 public class ContactosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactosApplication.class, args);
+		SpringApplication.run(ContactosApplication.class, args);		
 	}
 }
--- src/main/java/es/uma/informatica/sii/contactos/servicios/LogicaContactos.java
+++ src/main/java/es/uma/informatica/sii/contactos/servicios/LogicaContactos.java
@@ -4,22 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.uma.informatica.sii.contactos.entidades.Contacto;
 import es.uma.informatica.sii.contactos.repositorios.ContactoRepo;
 import es.uma.informatica.sii.contactos.servicios.excepciones.ContactoNoEncontrado;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
 
 @Service
 @Transactional
 public class LogicaContactos {
 	
 	private ContactoRepo repo;
+	private JmsTemplate jms;
 	
 	@Autowired
-	public LogicaContactos(ContactoRepo repo) {
+	public LogicaContactos(ContactoRepo repo, JmsTemplate jmsTemplate) {
 		this.repo=repo;
+		this.jms=jmsTemplate;
 	}
 	
 	public List<Contacto> getTodosContactos() {
@@ -28,7 +35,9 @@ public class LogicaContactos {
 	
 	public Contacto aniadirContacto(Contacto contacto) {
 		contacto.setId(null);
-		return repo.save(contacto);
+		contacto = repo.save(contacto);
+		jms.convertAndSend("contactos", "Añadido contacto");
+		return contacto; 
 	}
 	
 	public Optional<Contacto> getContactoPorId(Long id) {
@@ -37,7 +46,9 @@ public class LogicaContactos {
 	
 	public Contacto modificarContacto(Contacto contacto) {
 		if (repo.existsById(contacto.getId())) {
-			return repo.save(contacto);
+			contacto = repo.save(contacto);
+			jms.convertAndSend("contactos", "Modificado contacto");
+			return contacto;
 		} else {
 			throw new ContactoNoEncontrado();
 		}
@@ -46,6 +57,7 @@ public class LogicaContactos {
 	public void eliminarContacto(Long id) {
 		if (repo.existsById(id)) {
 			repo.deleteById(id);
+			jms.convertAndSend("contactos", "Eliminado contacto");
 		} else {
 			throw new ContactoNoEncontrado();
 		}
